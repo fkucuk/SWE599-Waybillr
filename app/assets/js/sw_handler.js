cc657f4dcb1141e8698d2501285878986313e2e7
Fatih Kucuk <fkucuk@gmail.com>
2017-12-17 12:57:25 +0200

    minor touches

--- app/assets/js/sw_handler.js
+++ app/assets/js/sw_handler.js
@@ -75,7 +75,7 @@ waybillrEvents.on('online', () => {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+        navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
@@ -109,18 +109,6 @@ var offlineDB = indexedDB.open("offlineDB", 1);
 offlineDB.onupgradeneeded = function () {
     var db = offlineDB.result;
     var store = db.createObjectStore("MyObjectStore", { keyPath: "id" });
-    // var queue = store.createIndex("NameIndex", [
-    //     "queue.url",
-    //     "queue.headers",
-    //     "queue.method",
-    //     "queue.mode",
-    //     "queue.credentials",
-    //     "queue.cache",
-    //     "queue.redirect",
-    //     "queue.referrer"
-    //  ]);
-
-    
 };
 
 offlineDB.onsuccess = function () {
@@ -130,34 +118,8 @@ offlineDB.onsuccess = function () {
     var store = tx.objectStore("MyObjectStore");
     
     console.log(store);
-    // var index = store.index("NameIndex");
-
-    // Add some data
-    // store.put({
-    //     id: 12345, queue: {
-    //         url: 'request.url',
-    //         headers: 'headers',
-    //         method: 'request.method',
-    //         mode: 'request.mode',
-    //         credentials: 'request.credentials',
-    //         cache: 'request.cache',
-    //         redirect: 'request.redirect',
-    //         referrer: 'request.referrer'
-    //     }});
     var crequest = store.count();
     crequest.onsuccess = function(e){
-        // store.add({
-        //     id: e.target.result+1, queue: {
-        //         url: 'request.url',
-        //         headers: 'headers',
-        //         method: 'request.method',
-        //         mode: 'request.mode',
-        //         credentials: 'request.credentials',
-        //         cache: 'request.cache',
-        //         redirect: 'request.redirect',
-        //         referrer: 'request.referrer'
-        //     }
-        // });
     }
   
     // Query the data
@@ -172,50 +134,3 @@ offlineDB.onsuccess = function () {
         db.close();
     };
 }
-
-// indexedDB
-
-// // This works on all devices/browsers, and uses IndexedDBShim as a final fallback 
-// var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
-
-// // Open (or create) the database
-// var open = indexedDB.open("MyDatabase", 1);
-
-// // Create the schema
-// open.onupgradeneeded = function () {
-//     var db = open.result;
-//     var store = db.createObjectStore("MyObjectStore", { keyPath: "id" });
-//     var index = store.createIndex("NameIndex", ["name.last", "name.first"]);
-// };
-
-// open.onsuccess = function () {
-//     // Start a new transaction
-//     var db = open.result;
-//     var tx = db.transaction("MyObjectStore", "readwrite");
-//     var store = tx.objectStore("MyObjectStore");
-//     var index = store.index("NameIndex");
-
-//     console.log(store);
-//     // Add some data
-//     store.put({ id: 12345, name: { first: "John", last: "Doe" }, age: 42 });
-//     store.put({ id: 67890, name: { first: "Bob", last: "Smith" }, age: 35 });
-
-//     // Query the data
-//     var getJohn = store.get(12345);
-//     var getBob = index.get(["Smith", "Bob"]);
-
-//     getJohn.onsuccess = function () {
-//         console.log(getJohn.result.name.first);  // => "John"
-//     };
-
-//     getBob.onsuccess = function () {
-//         console.log(getBob.result.name.first);   // => "Bob"
-//     };
-
-//     // Close the db when the transaction is done
-//     tx.oncomplete = function () {
-//         db.close();
-//     };
-// }
-
- 
